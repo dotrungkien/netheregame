310901cac17a3abbf29d913c7ec733eb69f5d7c7
Do Trung Kien <trungkien.keio@gmail.com>
2019-07-16 17:47:17 +0700

    implement block listening

--- Assets/Scripts/BlockNumber.cs
+++ Assets/Scripts/BlockNumber.cs
@@ -7,56 +7,65 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
-public class BlockNumber : MonoBehaviour {
+public class BlockNumber : MonoBehaviour
+{
     public Text blockNumberText;
     private float blockCheckRate = 3f;
     private float lastBlockCheckTime;
     private bool supportsProperThreads = false;
 
-    void Start () {
+    void Start()
+    {
         lastBlockCheckTime = 0f;
 
-        if (!supportsProperThreads) {
-            StartCoroutine (CheckBlockNumber ());
+        if (!supportsProperThreads)
+        {
+            StartCoroutine(CheckBlockNumber());
         }
     }
 
-    public IEnumerator CheckBlockNumber () {
+    public IEnumerator CheckBlockNumber()
+    {
         var wait = 1;
-        while (true) {
-            yield return new WaitForSeconds (wait);
+        while (true)
+        {
+            yield return new WaitForSeconds(wait);
             wait = 10;
-            var blockNumberRequest = new EthBlockNumberUnityRequest ("https://rinkeby.infura.io/v3/7238211010344719ad14a89db874158c");
-            yield return blockNumberRequest.SendRequest ();
-            if (blockNumberRequest.Exception == null) {
+            var blockNumberRequest = new EthBlockNumberUnityRequest("https://rinkeby.infura.io/v3/7238211010344719ad14a89db874158c");
+            yield return blockNumberRequest.SendRequest();
+            if (blockNumberRequest.Exception == null)
+            {
                 var blockNumber = blockNumberRequest.Result.Value;
-                blockNumberText.text = "Block: " + blockNumber.ToString ();
+                blockNumberText.text = "Block: " + blockNumber.ToString();
             }
         }
     }
 
-    public void Update () {
-        if (supportsProperThreads) {
+    public void Update()
+    {
+        if (supportsProperThreads)
+        {
             lastBlockCheckTime += Time.deltaTime;
 
-            if (lastBlockCheckTime >= blockCheckRate) {
+            if (lastBlockCheckTime >= blockCheckRate)
+            {
                 lastBlockCheckTime = 0f;
                 //CheckBlockNumberOnBackgroundThread();
             }
         }
     }
 
-    /* 
+    /*
     	public void CheckBlockNumberOnBackgroundThread()
     	{
-    		Task<HexBigInteger>.Run(async () => 
+    		Task<HexBigInteger>.Run(async () =>
     		{
     			return await web3.Eth.Blocks.GetBlockNumber.SendRequestAsync();
 
-    		}).ContinueWith( blockNumber => 
+    		}).ContinueWith( blockNumber =>
     		{
     			blockNumberText.text = "Block: " + blockNumber.Result.Value.ToString();
-    		}, 
+    		},
     		TaskScheduler.FromCurrentSynchronizationContext());
     	}
 
